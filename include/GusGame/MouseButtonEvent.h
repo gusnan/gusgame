0c02780928cfc560916e327def9cd335eb1f081f
Andreas Rönnquist <gusnan@gusnan.se>
2013-03-07 11:52:23 +0100

    Set freedungeon in copyleft block

--- include/GusGame/MouseButtonEvent.h
+++ include/GusGame/MouseButtonEvent.h
@@ -1,19 +1,19 @@
 /**
- *	This file is part of freedungeon
+ *	This file is part of GusGame
  *	Copyright (C) 2013 Andreas Rönnquist
  *
- *	freedungeon is free software: you can redistribute it and/or 
+ *	GusGame is free software: you can redistribute it and/or 
  *	modify it under the terms of the GNU General Public License as published 
  *	by the Free Software Foundation, either version 3 of the License, or
  *	(at your option) any later version.
  *
- *	freedungeon is distributed in the hope that it will be useful,
+ *	GusGame is distributed in the hope that it will be useful,
  *	but WITHOUT ANY WARRANTY; without even the implied warranty of
  *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *	GNU General Public License for more details.
  *
  *	You should have received a copy of the GNU General Public License
- *	along with freedungeon.  
+ *	along with GusGame.  
  *	If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef __HEADER_MOUSE_BUTTON_EVENT_
--- include/GusGame/StringHelper.h
+++ include/GusGame/StringHelper.h
@@ -1,19 +1,19 @@
 /**
- * This file is part of Damnation of the Gods
+ * This file is part of GusGame
  * Copyright 2008,2009 Andreas Rönnquist
  *
- * Damnation of the Gods is free software: you can redistribute it and/or 
+ * GusGame is free software: you can redistribute it and/or 
  * modify it under the terms of the GNU General Public License as published 
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * Damnation of the Gods is distributed in the hope that it will be useful,
+ * GusGame is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with Damnation of the Gods.  
+ * along with GusGame.  
  * If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef __HEADER_STRING_HELPER_
