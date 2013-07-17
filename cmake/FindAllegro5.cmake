66adfd2bb985b6df57a392749460a2bcd518229f
Andreas Rönnquist <gusnan@gusnan.se>
2013-07-17 15:12:10 +0200

    Fix finding allegro on Unix

--- cmake/FindAllegro5.cmake
+++ cmake/FindAllegro5.cmake
@@ -20,16 +20,16 @@ FIND_PATH(ALLEGRO_INCLUDE_DIR allegro.h
 set(VERSION_NUMBER "5.1")
 
 if(UNIX AND NOT CYGWIN)
-	exec_program(pkg-config ARGS "allegro-${VERSION_NUMBER} allegro_ttf-${VERSION_NUMBER} allegro_memfile-${VERSION_NUMBER} allegro_image-${VERSION_NUMBER} allegro_primitives-${VERSION_NUMBER} allegro_audio-${VERSION_NUMBER} allegro_acodec-${VERSION_NUMBER} --libs" OUTPUT_VARIABLE ALLEGRO_LIBRARY)
+	exec_program(pkg-config ARGS "allegro-${VERSION_NUMBER} allegro_ttf-${VERSION_NUMBER} allegro_memfile-${VERSION_NUMBER} allegro_image-${VERSION_NUMBER} allegro_primitives-${VERSION_NUMBER} allegro_audio-${VERSION_NUMBER} allegro_acodec-${VERSION_NUMBER} --libs" OUTPUT_VARIABLE ALLEGRO_LIBRARIES)
 	IF (NOT ${ALLEGRO_LIBRARIES})
 	    # FALL BACK TO 5.0
 	    set(VERSION_NUMBER "5.0")
-	    exec_program(pkg-config ARGS "allegro-${VERSION_NUMBER} allegro_ttf-${VERSION_NUMBER} allegro_memfile-${VERSION_NUMBER} allegro_image-${VERSION_NUMBER} allegro_primitives-${VERSION_NUMBER} allegro_audio-${VERSION_NUMBER} allegro_acodec-${VERSION_NUMBER} --libs" OUTPUT_VARIABLE ALLEGRO_LIBRARY)
-	ENDIF (NOT ${ALLEGRO_LIBRARY})
+	    exec_program(pkg-config ARGS "allegro-${VERSION_NUMBER} allegro_ttf-${VERSION_NUMBER} allegro_memfile-${VERSION_NUMBER} allegro_image-${VERSION_NUMBER} allegro_primitives-${VERSION_NUMBER} allegro_audio-${VERSION_NUMBER} allegro_acodec-${VERSION_NUMBER} --libs" OUTPUT_VARIABLE ALLEGRO_LIBRARIES)
+	ENDIF (NOT ${ALLEGRO_LIBRARIES})
 	# Check if any of these checks have found if so then cause failure)
 	STRING(REGEX MATCH "not found" LIB_CHECK ${ALLEGRO_LIBRARIES})
 	IF (${LIB_CHECK})
-	    set(ALLEGRO_LIBRARY "")
+	    set(ALLEGRO_LIBRARIES "")
 	ENDIF (${LIB_CHECK})
 # FIXME - Figure out what to do for windows 
 #else(UNIX AND NOT CYGWIN)
