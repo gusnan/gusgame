cd6f633171309354f58d1f579e03f91adebf41a6
Andreas Rönnquist <gusnan@gusnan.se>
2015-03-15 02:08:42 +0100

    Update copyrights

--- src/GraphicsLib/GraphicsHandler.cpp
+++ src/GraphicsLib/GraphicsHandler.cpp
@@ -1,7 +1,7 @@
 /**
  *
  *	This file is part of GusGame
- *	Copyright (C) 2013 Andreas Rönnquist
+ *	Copyright (C) 2013-2015 Andreas Rönnquist
  *
  *	GusGame is free software: you can redistribute it and/or
  *	modify it under the terms of the GNU General Public License as published
