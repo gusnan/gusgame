2b4859412d27fce0390c86ef18ea0573205408d5
Andreas Rönnquist <andreas@ronnquist.net>
2020-10-28 16:38:06 +0100

    Remove boost shared_ptr, use std

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -17,12 +17,11 @@
  *	along with GusGame.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-#include <boost/shared_ptr.hpp>
-
 #include <cstdlib>
 #include <iostream>
 #include <sstream>
 #include <list>
+#include <memory>
 
 #include "GusGame.h"
 
