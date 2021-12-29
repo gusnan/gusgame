3316a77c54c7e88727a4ce2c7de224be5a3fad62
Andreas Rönnquist <andreas@ronnquist.net>
2021-12-29 16:15:28 +0100

    Use make_shared instead of shared_ptr constructor

--- examples/example5.cpp
+++ examples/example5.cpp
@@ -143,7 +143,7 @@ int main(int argc,char **argv)
       // that is defined above
       EventSystem::initEventSystem();
 
-      eventHandler = std::shared_ptr<ExampleEventHandler>(new ExampleEventHandler());
+      eventHandler = std::make_shared<ExampleEventHandler>();
 
 
       // set the used EventHandler to the one we just created.
